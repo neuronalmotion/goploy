680147f214f5b56527826855f9e5df8e649a1a7a
Robin Penea <robin.penea@gmail.com>
2015-01-11 11:23:01 +0100

    Add uid/gid param per project
    Close #6

--- core/project.go
+++ core/project.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"os/exec"
 	"strings"
+    "syscall"
 )
 
 type Project struct {
 	Url    string `json:"url"`
 	Ref    string `json:"ref"`
 	Path   string `json:"path"`
+	Uid    int `json:"uid,omitempty"`
+	Gid    int `json:"gid,omitempty"`
 	Deploy string `json:"deploy,omitempty"`
 }
 
@@ -19,6 +22,21 @@ func (p *Project) UpdateRepo() error {
 		Args: []string{"git", "pull", p.Url},
 		Dir:  p.Path,
 	}
+
+    // gid has to be set before, if not, we might get
+    // a permission denied while setting uid
+    if p.Gid != 0 {
+        if err := syscall.Setgid(p.Gid); err != nil {
+            log.Fatalf("failed to Setgid(%d): %v", p.Gid, err)
+        }
+    }
+
+    if p.Uid != 0 {
+        if err := syscall.Setuid(p.Uid); err != nil {
+            log.Fatalf("failed to SetUid(%d): %v", p.Uid, err)
+        }
+    }
+
 	out, err := cmd.Output()
 	if err != nil {
 		log.Printf("%v", err)
